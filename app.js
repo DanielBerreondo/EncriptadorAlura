9e0e609fa51ae98e9e6cb8cd9ab4fd61910121b6
Daniel Berreondo <danielberreondo17@gmail.com>
2023-05-15 21:56:18 -0600

    Agregando Js y modificando etiquetas en html

---
+++ app.js
@@ -0,0 +1,15 @@
+/* 
+La letra 'e' es convertida en 'enter'
+La letra 'i' es convertida en 'imes'
+La letra 'a' es convertida en 'ai'
+La letra 'o' es convertida en 'ober'
+La letra 'u' es convertida en 'ufat'
+
+*/
+
+function prompt(texto) {
+    var texto = document.getElementById('prompt');
+    return texto.document
+}
+
+console.log(prompt());
