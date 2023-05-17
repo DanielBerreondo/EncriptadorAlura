6b7bb79e29e5adacf16f6368e1ce2d6cde40fe43
Daniel Berreondo <danielberreondo17@gmail.com>
2023-05-16 22:26:08 -0600

    Primer prototipo terminado del Encriptador

--- app.js
+++ app.js
@@ -7,9 +7,64 @@ La letra 'u' es convertida en 'ufat'
 
 */
 
-function prompt(texto) {
-    var texto = document.getElementById('prompt');
-    return texto.document
+const textArea = document.querySelector('#prompt');
+const mensaje = document.querySelector('.mensaje');
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
 }
 
-console.log(prompt());
+function btnCopiar() {
+    const textoCopiar = mensaje.value;
+    textArea.value = textoCopiar;
+
+}
+
+function encriptar(stringEncriptado) {
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringEncriptado = stringEncriptado.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptado;
+}
+
+function btnDesencriptar() {
+    const mensajeDesencriptado = desencriptar(textArea.value)
+    mensaje.value = mensajeDesencriptado;
+    mensaje.style.backgroundImage = "none";
+
+
+}
+
+function desencriptar(stringDesencriptar) {
+    let matrizCodigo = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringDesencriptar = stringDesencriptar.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptar.includes(matrizCodigo[i][1])) {
+            stringDesencriptar = stringDesencriptar.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return stringDesencriptar;
+}
