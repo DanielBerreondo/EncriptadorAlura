bd01c6bd5c708a865a8f9a6fa41f550b7e4d6b44
Daniel Berreondo <danielberreondo17@gmail.com>
2023-05-17 22:36:15 -0600

    Afinando logica del programa con Javascript

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ function encriptar(stringEncriptado) {
     stringEncriptado = stringEncriptado.toLowerCase();
 
     for (let i = 0; i < matrizCodigo.length; i++) {
-
         if (stringEncriptado.includes(matrizCodigo[i][0])) {
             stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
